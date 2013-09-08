1624c6adc35cbe8fd4c1d428d9ba6d349ba548bd
Fernando Valente <fernandovalentedev@gmail.com>
2013-09-08 12:27:40 -0300

    create first test file and update urls and core/view

--- eventex/core/tests/test_home.py
+++ eventex/core/tests/test_home.py
@@ -4,4 +4,12 @@ from django.test import TestCase
 
 
 class HomeTest(TestCase):
-    pass
+
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+
+    # def test_template(self):
+    #     self.assertTemplateUsed(self.response, 'home.html')
--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+# coding: utf-8
+
+from django.shortcuts import render, redirect
+
+
+def home(request):
+    """ Exibe a home com form para encurtar a URL """
+    return render(request, 'home.html')
